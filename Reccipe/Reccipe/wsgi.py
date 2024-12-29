ae72917f33ac72b4a8ba9ca0ce5d5645bfe61da9
Dhruv Jaiswal <dhruvujjain@gmail.com>
2024-12-29 23:31:38 +0530

    Fixed ALLOWED_HOSTS issue

--- Reccipe/Reccipe/wsgi.py
+++ Reccipe/Reccipe/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for Reccipe project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reccipe.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
+
+# Add WhiteNoise to serve static files
+application = WhiteNoise(application)
