c73b2483322e8f8e57b29515726f5246182a473c
Dhruv Jaiswal <dhruvujjain@gmail.com>
2024-12-29 23:23:10 +0530

    Fixed ALLOWED_HOSTS issue

--- Reccipe/Reccipe/settings.py
+++ Reccipe/Reccipe/settings.py
@@ -44,14 +44,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'Reccipe.urls'
 
@@ -122,6 +123,8 @@ import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIR = {
     os.path.join(BASE_DIR , "hotels/static")
