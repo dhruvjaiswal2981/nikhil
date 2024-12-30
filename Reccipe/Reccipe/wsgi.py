63a540791f52cce80344f542f7262788f0c626a3
Dhruv Jaiswal <dhruvujjain@gmail.com>
2024-12-30 16:09:55 +0530

    Fixed ALLOWED_HOSTS issue

--- Reccipe/Reccipe/wsgi.py
+++ Reccipe/Reccipe/wsgi.py
@@ -2,11 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reccipe.settings')
 
-# Get the WSGI application
 application = get_wsgi_application()
-
-# Add WhiteNoise to serve static files
-application = WhiteNoise(application)
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
--- Reccipe/recipe1/urls.py
+++ Reccipe/recipe1/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .import views
-
+from . import views
 
 urlpatterns = [
-    path('index/',views.index),
-    path('recipes/', views.recipes),
-    path('delete_recipe/<id>/',views.delete_recipe),#here dynamic url genrate using <id>
-    path('update_recipe/<id>/',views.update_recipe),
-    path('login/',views.login_page),
-    path('register/',views.register_page),
-    path('logout/',views.logout_page),
-
-    ]
+    path('', views.index, name='index'),  # Root URL points to index view
+    path('recipes/', views.recipes, name='recipes'),
+    path('delete_recipe/<id>/', views.delete_recipe, name='delete_recipe'),
+    path('update_recipe/<id>/', views.update_recipe, name='update_recipe'),
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
+    path('logout/', views.logout_page, name='logout'),
+]
